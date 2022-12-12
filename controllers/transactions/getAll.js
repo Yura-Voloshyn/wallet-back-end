b2a90e9f701c08abd7243ceeba0a1385112ef223
Olha Truba <o.ljolik@gmail.com>
2022-12-12 18:19:05 +0200

    Update getAll.js

--- controllers/transactions/getAll.js
+++ controllers/transactions/getAll.js
@@ -4,6 +4,8 @@ const getAll = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 5, ...filter } = req.query;
   const skip = (page - 1) * limit;
+  const length = (await Transaction.find({ owner })).length;
+  const totalPages = Math.ceil(length / limit);
 
   const result = await Transaction.find(
     { owner, ...filter },
@@ -20,6 +22,8 @@ const getAll = async (req, res) => {
     status: "success",
     code: 200,
     data: {
+      page: +page,
+      totalPages,
       result
     }
   });
